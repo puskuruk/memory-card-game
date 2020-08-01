7ed607cff76ca0c1d3430a28a15e86f6411a0c51
Puskuruk <tamayeseruysal@gmail.com>
2020-08-01 17:06:59 +0300

    Initial Commit

---
+++ Memorize-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize-2
+//
+//  Created by Tamay Eser Uysal on 1.08.2020.
+//  Copyright © 2020 Tamay Eser Uysal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
